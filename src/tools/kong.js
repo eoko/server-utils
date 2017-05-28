8791306506f9d1475d2ae278129cf53d9d1e7c5f
merlin <romain.dary@eoko.fr>
2017-05-29 00:52:00 +0200

    add url for kong

--- src/tools/kong.js
+++ src/tools/kong.js
@@ -2,9 +2,9 @@ const debug    = require('debug')('server:mongo');
 const registry = require('@eoko/service-registry');
 const kong     = require('@eoko/kong-client');
 
-module.exports = () => {
-  const uri = registry.getService('kong-8001')
-                      .getUri();
-  kong.getKong()
-      .setBaseUrl(uri);
+module.exports = (url) => {
+  return () => {
+    const uri = url || registry.getService('kong-8001').getUri();
+    kong.getKong().setBaseUrl(uri);
+  }
 };
