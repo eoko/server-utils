be470d9c70e311b57517f256780ddf397cb21429
merlin <romain.dary@eoko.fr>
2017-05-28 23:45:15 +0200

    fix body

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose     = require('./src/tools/mongo');
 const registry     = require('./src/tools/registry');
 const sentry       = require('./src/tools/sentry');
 const requestRetry = require('./src/utils/requestRetry');
+const kong         = require('./src/tools/kong');
 
-module.exports.tools = { mongoose, registry, sentry };
+module.exports.tools = { mongoose, registry, sentry, kong };
 module.exports.utils = { requestRetry };
